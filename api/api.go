a79f637c35ebaef6ce28ce85683ce2498c54e794
Anton Chernov2 <anton_chernov2@epuavinw0183.kyiv.epam.com>
2020-02-17 17:16:09 +0200

    Add pagination not full but a little bit

--- api/api.go
+++ api/api.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	utl "siteparser/utils"
+	"strconv"
 	"time"
 )
 
@@ -36,17 +37,28 @@ func GetUserEmails(w http.ResponseWriter, r *http.Request) {
 	// 	// log.Println("ID:", *db)
 	// }
 	emailsArray := make([]string, 0)
+	limit, lerr := strconv.Atoi(r.FormValue("limit"))
+	offset, oerr := strconv.Atoi(r.FormValue("offset"))
+	if lerr != nil {
+		limit = 20
+	}
+	if oerr != nil {
+		offset = 0
+	}
+	log.Println("LIMIT", limit)
+	log.Println("OFFSET", offset)
 
 	// defer db.Close()
 	query := `
 		SELECT email
 		FROM authenticate_customuser
 		WHERE email !='' OR email !=null
-		LIMIT 20 
-		OFFSET 0 
+		LIMIT $1 
+		OFFSET $2 
 	`
 
-	data, err := utl.DB.Query(query)
+	data, err := utl.DB.Query(query, limit, offset)
+	defer data.Close()
 
 	if err != nil {
 		log.Println("Db Query error: ", err)
@@ -73,8 +85,8 @@ func GetUserEmails(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//InterfaceCreateNewUser New use structure
-type InterfaceCreateNewUser struct {
+//StructCreateNewUser New use structure
+type StructCreateNewUser struct {
 	Password       string    `json:"password"`
 	IsSuperuser    bool      `json:"is_superuser"`
 	Email          string    `json:"email"`
@@ -87,16 +99,69 @@ type InterfaceCreateNewUser struct {
 	AgentsIsActive bool      `json:"agents_is_active"`
 }
 
-//CreateNewUser Create a new user
-func CreateNewUser(w http.ResponseWriter, r *http.Request) {
-	db, err := utl.PostgressDBConnection()
+//DetailUserDataStruct detail user data
+type DetailUserDataStruct struct {
+	ID             int       `json:"id"`
+	IsSuperuser    bool      `json:"is_superuser"`
+	Email          string    `json:"email"`
+	Phone          string    `json:"phone"`
+	IsStaff        bool      `json:"is_staff"`
+	IsActive       bool      `json:"is_active"`
+	DateJoined     time.Time `json:"date_joined"`
+	DateUpdate     time.Time `json:"date_update"`
+	EmailConfirm   bool      `json:"email_confirm"`
+	AgentsIsActive bool      `json:"agents_is_active"`
+}
 
-	if err != nil {
-		log.Println("No connection to db! Error:", err)
-	} else {
-		log.Println("Get DB connection!")
+//GetDetailUsersData get detail data for all users
+func GetDetailUsersData(w http.ResponseWriter, r *http.Request) {
+
+	limit, lerr := strconv.Atoi(r.FormValue("limit"))
+	offset, oerr := strconv.Atoi(r.FormValue("offset"))
+	if lerr != nil {
+		limit = 20
+	}
+	if oerr != nil {
+		offset = 0
+	}
+	log.Println("LIMIT", limit)
+	log.Println("OFFSET", offset)
+	query := `
+	SELECT (id, email, phone, is_active, date_joined, date_updated)
+	FROM authenticate_customuser
+	LIMIT $1 
+	OFFSET $1 
+	`
+
+	data, err := utl.DB.Query(query, limit, offset)
+
+	for data.Next() {
+
 	}
 
-	defer db.Close()
+	if err != nil {
+		log.Println("Error", err)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
 
+		json.NewEncoder(w).Encode(data)
+	}
 }
+
+//CreateNewUser Create a new user
+// func CreateNewUser(w http.ResponseWriter, r *http.Request) {
+
+// 	// READ data from request must be added!!!
+
+// 	query := `
+// 		INSERT INTO authenticate_customuser ()
+// 		VALUES authenticate_customuser
+// 	`
+// 	data, err := utl.DB.Exeq(query)
+// 	if err != nil {
+// 		log.Println("Error", err)
+// 	} else {
+// 		log.Println("Log data", data)
+// 	}
+
+// }
